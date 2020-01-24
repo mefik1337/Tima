038cf318fbb48f1c5763397cfee7680dde7b715d
Mateusz Szczęch <szczechmatt@gmail.com>
2020-01-24 13:45:40 +0100

    add simple rwd

--- src/js/components/NavDashboard.js
+++ src/js/components/NavDashboard.js
@@ -6,10 +6,17 @@ export default class NavDashboard extends Component {
     state = {
         name: "",
         surname: "",
+        isopen: false
     };
 
     handleLogOut = () => {
         fire.auth().signOut()
+    };
+
+    handleBurger = () => {
+        this.setState(prevState => ({
+            isopen: !prevState.isopen
+        }));
     };
 
     componentDidMount() {
@@ -34,21 +41,25 @@ export default class NavDashboard extends Component {
         const noPermission = <NavLink to="/NoPermission" activeClassName="nav-chosen"><i className="fa fa-thumb-tack" aria-hidden="true"></i>Add tasks</NavLink>;
         const checkTheLevel = this.props.level > 1 ? addTasks : noPermission;
         return (
-            <nav>
+            <>
+            <button className="hidden-hamburger" onClick={this.handleBurger}>{this.state.isopen ?
+                <i className="fa fa-bars"></i> : <i className="fa fa-times"></i>}</button>
+            <nav className={this.state.isopen ? "hidden" : null}>
                 <div className="container">
                     <div className="navbar-row">
                         <span className="nav-title">Tima <span>Dashboard</span></span>
                         <span className="nav-welcome">Hi, <span>{this.state.name} {this.state.surname}</span></span>
-                        <span className="nav-links">
+                        <div className="nav-links">
                             <NavLink to="/content" activeClassName="nav-chosen"><i className="fa fa-home"></i>Dashboard</NavLink>
                             <NavLink to="/chat" activeClassName="nav-chosen"><i className="fa fa-commenting"></i>Chat</NavLink>
                             {checkTheLevel}
                             <button onClick={this.handleLogOut} className="logoutBtn"><i className="fa fa-sign-out"></i>Logout</button>
-                        </span>
+                        </div>
 
                     </div>
                 </div>
             </nav>
+            </>
         )
     }
 }
